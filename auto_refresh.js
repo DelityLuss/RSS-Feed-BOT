90324f5a375363e237585cb3c70fe66e450c6e5c
Luss <luss.delity@gmail.com>
2023-06-04 18:07:13 +0200

    init

--- auto_refresh.js
+++ auto_refresh.js
@@ -1,5 +1,24 @@
 // Path : auto_refresh.js
 const getCalendar = require("./src/getCalendar");
 
-getCalendar("2022-09-05", "2023-06-16");
+// refresh the calendar
+async function refreshCalendar() {
+        
+        date = new Date();
+    
+        firstDay = "2022-09-05"
+        lastDay = "2023-06-16"
+    
+        console.log("refreshing calendar from " + firstDay + " to " + lastDay);
+    
+        await getCalendar(firstDay, lastDay);
+    
+
+        console.log('Calendar refreshed! from ' + firstDay + ' to ' + lastDay);
+    
+    }
+
+setInterval(refreshCalendar, 1000 * 60 * 60 * 24); // refresh every day
+
+
 
