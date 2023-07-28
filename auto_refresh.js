dc2015242715823b0fa203da7ff84325e77efbf0
Luss <eliot.cherpin@etu.univ-grenoble-alpes.fr>
2023-07-29 00:25:34 +0200

    update

--- auto_refresh.js
+++ auto_refresh.js
@@ -4,6 +4,9 @@ var CronJob = require('cron').CronJob;
 
 const getCalendar = require("./src/getCalendar");
 
+firstDay = "2022-09-05"
+lastDay = "2023-06-16"
+
 var job = new CronJob('0 0 * * * *', async function () {
     console.info("[+] Auto-Refreshing calendar from " + firstDay + " to " + lastDay);
     await getCalendar("all", firstDay, lastDay);
