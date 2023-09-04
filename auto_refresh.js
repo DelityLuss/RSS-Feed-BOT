204c25d70e0dadca4f344d2c06b6990c41b9abc2
Luss <86884006+DelityLuss@users.noreply.github.com>
2023-09-04 21:02:24 +0300

    Update auto_refresh.js

--- auto_refresh.js
+++ auto_refresh.js
@@ -4,8 +4,8 @@ var CronJob = require('cron').CronJob;
 
 const getCalendar = require("./src/getCalendar");
 
-firstDay = "2022-09-05"
-lastDay = "2023-06-16"
+firstDay = "2023-09-04"
+lastDay = "2024-06-30"
 
 var job = new CronJob('0 0 * * * *', async function () {
     console.info("[+] Auto-Refreshing calendar from " + firstDay + " to " + lastDay);
