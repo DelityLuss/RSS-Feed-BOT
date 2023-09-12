3f141a52b332a8b2ba94277e4404181a0209c7db
Aekyios <contact@aekyios.fr>
2023-09-12 18:42:29 +0200

    patch refresh

--- auto_refresh.js
+++ auto_refresh.js
@@ -7,9 +7,9 @@ const getCalendar = require("./src/getCalendar");
 firstDay = "2023-09-04"
 lastDay = "2024-06-30"
 
-var job = new CronJob('0 0 * * * *', async function () {
+var job = new CronJob('17 30 * * * *', async function () {
     console.info("[+] Auto-Refreshing calendar from " + firstDay + " to " + lastDay);
-    await getCalendar("all", firstDay, lastDay);
+    await getCalendar("ALL", firstDay, lastDay);
     }
     , null, true, 'Europe/Paris');
 
