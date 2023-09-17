4cdd9140da56e8d2c82da205e281edbb1c58aeb8
Aekyios <contact@aekyios.fr>
2023-09-17 22:58:16 +0200

    up

--- auto_refresh.js
+++ auto_refresh.js
@@ -9,7 +9,13 @@ lastDay = "2024-06-30"
 
 var job = new CronJob('0 30 17 * * *', async function () { //effectue l'update tous les jours à 17h30 et 0 secondes
     console.info("[+] Auto-Refreshing calendar from " + firstDay + " to " + lastDay);
-    await getCalendar("ALL", firstDay, lastDay);
+    await getCalendar("B1G1", firstDay, lastDay);
+    await getCalendar("B1G2", firstDay, lastDay);
+    await getCalendar("B1G3", firstDay, lastDay);
+    await getCalendar("B1G4", firstDay, lastDay);
+    await getCalendar("B2G1", firstDay, lastDay);
+    await getCalendar("B2G2", firstDay, lastDay);
+    await getCalendar("B2GA", firstDay, lastDay);
     }
     , null, true, 'Europe/Paris');
 
